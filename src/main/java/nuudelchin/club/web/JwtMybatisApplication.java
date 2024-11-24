506a7994ed82915c09c18087f11fbe72bf7d0e38
lkhaajav <lkhaching@gmail.com>
2024-11-24 21:39:22 +0900

    init code

---
+++ src/main/java/nuudelchin/club/web/JwtMybatisApplication.java
@@ -0,0 +1,13 @@
+package nuudelchin.club.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtMybatisApplication.class, args);
+	}
+
+}
