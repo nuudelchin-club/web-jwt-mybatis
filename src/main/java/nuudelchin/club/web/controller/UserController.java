4387614f42e5a9cb248cee85a88c0ca592756963
lkhaajav <lkhaching@gmail.com>
2024-12-12 10:31:27 +0900

    userData API

---
+++ src/main/java/nuudelchin/club/web/controller/UserController.java
@@ -0,0 +1,47 @@
+package nuudelchin.club.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import nuudelchin.club.web.entity.UserEntity;
+import nuudelchin.club.web.service.UserService;
+
+@Controller
+public class UserController {
+	
+	private final UserService userService;
+	
+	public UserController(UserService userService) {
+		
+		this.userService = userService;
+	}
+
+	@GetMapping("/user")
+	@ResponseBody
+	public Object user() {
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		String username = authentication.getName();
+		
+		UserEntity userEntity = userService.findByUsername(username);
+		
+		Map<String, Object> result = new HashMap<String, Object>(); 
+		
+		if(userEntity != null) {
+			
+			String role = userEntity.getRole();
+			
+			result.put("username", username);
+			result.put("role", role);
+		}
+		System.out.println(result);		
+		return result;
+	}
+}
